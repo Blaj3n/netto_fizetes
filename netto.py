470cd8eeb07a3dbbec22a9e5b3dc99f4716bda24
Blaj3n <benceulrich@gmail.com>
2024-10-10 11:35:46 +0200

    formázással kiegészítve, további fejlesztés(ek) javasoltak

--- netto.py
+++ netto.py
@@ -1,3 +1,6 @@
+# TOVÁBBFEJLESZTÉSI LEHETŐSÉGEK:
+# 25 év alatti szja kedvezmény
+
 brutto_fizetes = int(input("Kérem adja meg bruttóban jelenlegi fizetését: "))
 
 szja = round(brutto_fizetes * 0.15)
@@ -5,9 +8,9 @@ tb = round(brutto_fizetes * 0.185)
 
 osszes_levonas = szja + tb
 
-print(f"Személyi Jövedelemadó(15%): {szja}")
-print(f"Társadalombiztosítási Járulék(18,5%): {tb}")
-print(f"Levonások összesen: {osszes_levonas}")
+print(f"Személyi Jövedelemadó(15%): \t\t\t{szja:,.0f}".replace(",", "."))
+print(f"Társadalombiztosítási Járulék(18,5%): \t{tb:,.0f}".replace(",", "."))
+print(f"Levonások összesen: \t\t\t\t\t{osszes_levonas:,.0f}".replace(",", "."))
 
 netto_fizetes = brutto_fizetes - osszes_levonas
-print(f"Nettó fizetése: {netto_fizetes}")
+print(f"Nettó fizetése: \t\t\t\t\t\t{netto_fizetes:,.0f}".replace(",", "."))
